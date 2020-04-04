aa39b5bb097a9edb12ffbcd439ff1dca7a87f86f
jshaffer5 <joshtwoone@gmail.com>
2020-04-04 10:52:25 -0700

    Update 8. spread operator.js

--- 8. spread operator.js
+++ 8. spread operator.js
@@ -1,1 +1,11 @@
+// ES6 introduces the spread operator, which allows us to expand arrays and other 
+// expressions by unpacking them into places where multiple parameters or elements are expected.
 
+function frankenSplice(arr1, arr2, n) {
+  let combined_arr = [...arr2];
+  combined_arr.splice(n, 0, ...arr1); // splice( starting index, # items to remove, items... )
+
+  return combined_arr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
