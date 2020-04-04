8cf10c033bf635f38e7c794994a741f03c4d2661
jshaffer5 <joshtwoone@gmail.com>
2020-04-04 10:59:34 -0700

    Create 9. destructuring assignment.js

---
+++ 9. destructuring assignment.js
@@ -0,0 +1,12 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// ES6 code with destructuring
+const { today, tomorrow } = HIGH_TEMPERATURES
+
+// ES5 code, without destructuring assignment:
+const today = HIGH_TEMPERATURES.today;
+const tomorrow = HIGH_TEMPERATURES.tomorrow;
