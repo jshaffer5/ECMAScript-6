7c1597fadae080343d7ee88a22e049876cf051c2
jshaffer5 <joshtwoone@gmail.com>
2020-04-20 00:07:40 -0700

    Update 3-4. using const.js

--- 3-4. using const.js
+++ 3-4. using const.js
@@ -1,5 +1,5 @@
 // using const for declaring a variable only prevents reassignment of object
 const s = [5, 6, 7];
 s = [1, 2, 3]; // throws error, trying to assign a const
-s[2] = 45; // works just as it would with an array declared with var or let
+s[2] = 45; // works fine as the object is only being edited, not reassigned
 console.log(s); // returns [5, 6, 45]
